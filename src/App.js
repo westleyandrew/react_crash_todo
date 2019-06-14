dd915fb83326ed02607763af9881d571305e3c60
Westley Andrew <westleyandrew@hotmail.com>
2019-06-14 18:13:24 +0100

    First commit for React Crash Todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos'
 
 function App() {
+  let state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Watch Aquaman',
+        completed: false
+      },
+    ]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos todos={state.todos} />
     </div>
   );
 }
